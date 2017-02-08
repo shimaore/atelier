e219e349dd9a0826d46731480f92e316211b4bef
Stephane Alnet <stephane@shimaore.net>
2017-02-08 22:01:22 +0100

    localhost

--- test.js
+++ test.js
@@ -4,14 +4,13 @@ var request = require('superagent')
 var co = require('co')
 var fun1G = function*(){
   var p, q ,r, res
-  res = yield request.get('https://google.com')
+  res = yield request.get('http://localhost:8080')
   p = res.text
-  res = yield request
-    .get('https://facebook.com')
+  res = yield request.get('http://localhost:8080')
   // .catch(function(){ return null })
   // if(!res) return 67
   q = res.text
-  res = yield request.get('https://yahoo.com')
+  res = yield request.get('http://localhost:8080')
   r = res.text
   console.log(p+q+r)
   return 42
