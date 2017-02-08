eb5ce95d10020af0ef8896f450c9b466b04a4565
Stephane Alnet <stephane@shimaore.net>
2017-02-08 22:17:39 +0100

    parallel & sequential

--- test.js
+++ test.js
@@ -4,13 +4,13 @@ var request = require('superagent')
 var co = require('co')
 var fun1G = function*(){
   var p, q ,r, res
-  res = yield request.get('http://localhost:8080')
-  p = res.text
-  res = yield request.get('http://localhost:8080')
-  // .catch(function(){ return null })
-  // if(!res) return 67
-  q = res.text
-  res = yield request.get('http://localhost:8080')
+  res = yield Promise.all([
+    request.get('http://localhost:8080')
+  , request.get('http://localhost:8080')
+  ])
+  p = res[0].text
+  q = res[1].text
+  res = yield request.get('http://localhost:8080'+q)
   r = res.text
   console.log(p+q+r)
   return 42
